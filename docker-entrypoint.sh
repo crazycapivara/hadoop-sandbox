1e87d3c2171788cfed5ea4e39076168c99fffc1a
stkuethe <crazycapivara@gmail.com>
2017-11-20 15:00:17 +0000

    Add rsa key for 0.0.0.0 to 'known_hosts'

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 /usr/sbin/sshd
-#ssh-keyscan -H localhost >> ~/.ssh/known_hosts
-ssh-keyscan -t rsa -H localhost >> ~/.ssh/known_hosts 
+ssh-keyscan -t rsa -H localhost >> ~/.ssh/known_hosts
+# needed for secondary NameNode 
+ssh-keyscan -t rsa -H 0.0.0.0 >> ~/.ssh/known_hosts 
 exec "$@"
-#/bin/bash
 
